a2f5c01a335dd55081b62d96c95ffc554578f06f
Dan Tran <picasdan99@gmail.com>
2022-03-14 20:38:32 +0700

    Fix bug in displayed data

--- binary_choice_game/views/__init__.py
+++ binary_choice_game/views/__init__.py
@@ -7,6 +7,7 @@ from otree.common import get_app_label_from_import_path
 from binary_choice_game.recommendations import NoneRecommender, get_recommender
 from binary_choice_game.utils import timestamp2utcdatetime, try_else_none
 
+
 class CustomPage(Page):
     def get_template_name(self):
         if self.template_name is not None:
@@ -40,7 +41,7 @@ class QnPage(CustomPage):
     @staticmethod
     def live_method(player: Player, data: dict):
         logger = logging.getLogger(__name__)
-        
+
         logger.info(f"Received data from player {player.id_in_group}: {data}.")
 
         if is_finished(player):
@@ -48,7 +49,8 @@ class QnPage(CustomPage):
             return {player.id_in_group: dict(is_finished=True)}
 
         if data:
-            logger.info(f"Getting current trial for player {player.id_in_group}.")
+            logger.info(
+                f"Getting current trial for player {player.id_in_group}.")
             trial = get_current_trial(player)
             logger.info(f"Current trial: {trial}.")
 
@@ -65,7 +67,7 @@ class QnPage(CustomPage):
         logger.info(f"Getting next trial for player {player.id_in_group}.")
         trial = get_current_trial(player)
         logger.info(f"Next trial: {trial}")
-        
+
         recommender = get_recommender(player.treatment)
         if trial.rec == None and not isinstance(recommender, NoneRecommender):
             trial.rec = recommender.rec(player, (trial.optionA, trial.optionB))
@@ -77,35 +79,42 @@ class QnPage(CustomPage):
                 rec=trial.rec,
             )
         }
-        logger.info(f"Next trial for player {player.id_in_group}: {next_trial_data}")
+        logger.info(
+            f"Next trial for player {player.id_in_group}: {next_trial_data}")
 
         return next_trial_data
+
+
+def get_derived_data(trial: Trial):
+    rec = None if trial.rec == None else int(trial.rec)
+    response = None if trial.response == None else int(trial.response)
+    start_timestamp_ms = int(trial.start_str_timestamp_ms)
+    end_timestamp_ms = int(trial.end_str_timestamp_ms)
+    return {
+        **trial.__dict__,
+        **dict(
+            rec=rec,
+            response=response,
+            utc_start_time=timestamp2utcdatetime(start_timestamp_ms),
+            utc_end_time=timestamp2utcdatetime(end_timestamp_ms),
+            time_spent_ms=try_else_none(
+                lambda: end_timestamp_ms
+                - start_timestamp_ms
+            ),
+        ),
+    }
 
 
 class Results(CustomPage):
     @staticmethod
     def vars_for_template(player: Player):
+        logger = logging.getLogger(__name__)
+        logger.info("Retrieving trial data for display.")
+
         trials = Trial.filter(player=player)
-        trials = [
-            {
-                **trial.__dict__,
-                **dict(
-                    rec=None if trial.rec == None else int(trial.rec),
-                    response=None
-                    if trial.response == None
-                    else int(trial.response),
-                    utc_start_time=timestamp2utcdatetime(
-                        trial.start_str_timestamp_ms
-                    ),
-                    utc_end_time=timestamp2utcdatetime(trial.end_str_timestamp_ms),
-                    time_spent_ms=try_else_none(
-                        lambda: trial.end_str_timestamp_ms
-                        - trial.start_str_timestamp_ms
-                    ),
-                ),
-            }
-            for trial in trials
-        ]
+        trials = [get_derived_data(trial) for trial in trials]
+        logger.info(f"Trials: {trials}")
+
         return dict(trials=trials)
 
 
