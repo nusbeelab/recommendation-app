6875bef85a0971abadddb2968c4beb19c80717bb
Dan Tran <58513950+picasdan9@users.noreply.github.com>
2022-03-20 14:53:07 +0700

    Fix UI bugs (#53)

--- binary_choice_game/views/__init__.py
+++ binary_choice_game/views/__init__.py
@@ -27,12 +27,7 @@ class StartPage(GamePage):
 
 
 def get_current_trial(player: Player):
-    try:
-        return Trial.filter(player=player, button=None)[0]
-
-    except Exception as err:
-        logging.getLogger(__name__).error(err)
-        raise err
+    return Trial.filter(player=player, button=None)[0]
 
 
 def is_finished(player: Player):
@@ -49,58 +44,60 @@ class QnPage(GamePage):
     @staticmethod
     def live_method(player: Player, data: dict):
         logger = logging.getLogger(__name__)
-
         logger.info(f"Received data from player {player.id_in_group}: {data}.")
+        try:
+            if is_finished(player):
+                logger.info(f"Player {player.id_in_group} has finished.")
+                return {player.id_in_group: dict(is_finished=True)}
 
-        if is_finished(player):
-            logger.info(f"Player {player.id_in_group} has finished.")
-            return {player.id_in_group: dict(is_finished=True)}
+            if data:
+                logger.info(f"Getting current trial for player {player.id_in_group}.")
+                trial = get_current_trial(player)
+                logger.info(f"Current trial: {trial}.")
 
-        if data:
-            logger.info(f"Getting current trial for player {player.id_in_group}.")
+                # trial.response = 0 if data.get("response") == 'L' and trial.
+                trial.button = data.get("button")
+                trial.start_str_timestamp_ms = str(data.get("start_timestamp_ms"))
+                trial.end_str_timestamp_ms = str(data.get("end_timestamp_ms"))
+                player.num_completed += 1
+                logger.info(f"Recorded trial: {trial}")
+
+            if is_finished(player):
+                logger.info(f"Player {player.id_in_group} has finished.")
+                return {player.id_in_group: dict(is_finished=True)}
+
+            logger.info(f"Getting next trial for player {player.id_in_group}.")
             trial = get_current_trial(player)
-            logger.info(f"Current trial: {trial}.")
+            logger.info(f"Next trial: {trial}")
 
-            # trial.response = 0 if data.get("response") == 'L' and trial.
-            trial.button = data.get("button")
-            trial.start_str_timestamp_ms = str(data.get("start_timestamp_ms"))
-            trial.end_str_timestamp_ms = str(data.get("end_timestamp_ms"))
-            player.num_completed += 1
-            logger.info(f"Recorded trial: {trial}")
+            # give recommendations here
+            trial.rec = None
 
-        if is_finished(player):
-            logger.info(f"Player {player.id_in_group} has finished.")
-            return {player.id_in_group: dict(is_finished=True)}
+            lottery_pair = C.LOT_PAIR_MANAGER.convert_ids_to_lottery_pairs(
+                [trial.problem_id]
+            )[0]
+            left_option = unpack_lottery(lottery_pair.a)
+            right_option = unpack_lottery(lottery_pair.b)
+            if trial.left_option == 1:
+                left_option, right_option = right_option, left_option
 
-        logger.info(f"Getting next trial for player {player.id_in_group}.")
-        trial = get_current_trial(player)
-        logger.info(f"Next trial: {trial}")
-
-        # give recommendations here
-        trial.rec = None
-
-        lottery_pair = C.LOT_PAIR_MANAGER.convert_ids_to_lottery_pairs(
-            [trial.problem_id]
-        )[0]
-        left_option = unpack_lottery(lottery_pair.a)
-        right_option = unpack_lottery(lottery_pair.b)
-        if trial.left_option == 1:
-            left_option, right_option = right_option, left_option
-
-        next_trial_data = {
-            player.id_in_group: dict(
-                left_option=left_option,
-                right_option=right_option,
-                rec=trial.rec,
-                progress=player.num_completed
-                / C.NUM_TRIALS_BY_STAGE[player.round_number],
+            next_trial_data = {
+                player.id_in_group: dict(
+                    left_option=left_option,
+                    right_option=right_option,
+                    rec=trial.rec,
+                    progress=player.num_completed
+                    / C.NUM_TRIALS_BY_STAGE[player.round_number],
+                )
+            }
+            logger.info(
+                f"Next trial data for player {player.id_in_group}: {next_trial_data}"
             )
-        }
-        logger.info(
-            f"Next trial data for player {player.id_in_group}: {next_trial_data}"
-        )
 
-        return next_trial_data
+            return next_trial_data
+        except Exception as err:
+            logger.error(err)
+            raise err
 
 
 class EndPage(GamePage):
@@ -116,20 +113,23 @@ class Results(GamePage):
     def vars_for_template(player: Player):
         logger = logging.getLogger(__name__)
         logger.info("Retrieving trial data for display.")
+        try:
+            trials = Trial.filter(player=player)
+            trials = [
+                {
+                    header: data
+                    for header, data in zip(
+                        C.DATA_EXPORT_HEADERS, get_data_export_row(player, trial)
+                    )
+                }
+                for trial in trials
+            ]
+            logger.info(f"Trials: {trials}")
 
-        trials = Trial.filter(player=player)
-        trials = [
-            {
-                header: data
-                for header, data in zip(
-                    C.DATA_EXPORT_HEADERS, get_data_export_row(player, trial)
-                )
-            }
-            for trial in trials
-        ]
-        logger.info(f"Trials: {trials}")
-
-        return dict(trials=trials)
+            return dict(trials=trials)
+        except Exception as err:
+            logger.error(err)
+            raise err
 
 
 PRE_EXPERIMENT_SEQ = [
